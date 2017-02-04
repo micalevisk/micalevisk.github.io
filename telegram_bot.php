836928307539bfafdd9df78bee4547884842acc8
micalevisk <mllc@icomp.ufam.edu.br>
2017-02-04 17:01:27 -0400

    teste bot Telegram

---
+++ telegram_bot.php
@@ -0,0 +1,39 @@
+<?php
+/*
+https://api.telegram.org/bot328107801:AAGlUIukOJqJy4P_iPA-HiPnkVJ8awPH4lM/getupdates
+https://api.telegram.org/bot328107801:AAGlUIukOJqJy4P_iPA-HiPnkVJ8awPH4lM/getme
+https://api.telegram.org/bot328107801:AAGlUIukOJqJy4P_iPA-HiPnkVJ8awPH4lM/sendmessage?chat_id=170449962&text=<>
+*/
+
+
+$botToken = "328107801:AAGlUIukOJqJy4P_iPA-HiPnkVJ8awPH4lM";
+$website = "https://api.telegram.org/bot".$botToken;
+
+
+$update = file_get_contents($website."/getupdates");
+print_r($update);
+/*
+$update = file_get_contents('php://input');
+$update = json_decode($update, TRUE);
+
+$chatId = $update["message"]["chat"]["id"];
+$message = $update["message"]["text"];
+
+switch($message) {
+	case "/falar":
+		sendMessage($chatId, "odeio doritos!!");
+		break;
+	default: 
+		sendMessage($chatId, "default");
+}
+
+function sendMessage ($chatId, $message) {
+	$url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text=".urlencode($message);
+	file_get_contents($url);
+}
+*/
+
+
+
+
+?>
